d9ccf7b869f2f16a685ae3a425577527a7743785
shaharsol <shahar@gnift.com>
2024-09-09 16:43:35 +0300

    useState

--- northwind/src/components/home/home/Home.tsx
+++ northwind/src/components/home/home/Home.tsx
@@ -1,12 +1,14 @@
 import './Home.css'
 import products1Source from '../../../assets/images/products1.jpeg'
 import products2Source from '../../../assets/images/products2.jpeg'
+import { useState } from 'react'
 
 
 
 function Home(): JSX.Element {
     
-    const imageNumber = Math.round(Math.random()) + 1
+    // const imageNumber = Math.round(Math.random()) + 1
+    const [ imageNumber ] = useState<number>(Math.round(Math.random()) + 1)
 
     const desserts = [
         {id: 1, name: 'pana cota', price: 20},
@@ -14,6 +16,20 @@ function Home(): JSX.Element {
         {id: 3, name: 'bavaria', price: 25},
         {id: 4, name: 'baqlawa', price: 10},
     ]
+
+    const sale1Info = '20% on all drinks'
+    const sale2Info = '15% on all desserts'
+    const [ saleInfo, setSaleInfo ] = useState<string>(sale1Info)
+
+    function displaySale1Info() {
+        console.log('in function displaySale1Info')
+        setSaleInfo(sale1Info)
+    }
+
+    function displaySale2Info() {
+        console.log('in function displaySale2Info')
+        setSaleInfo(sale2Info)
+    }
 
     return (
         <div className='Home'>
@@ -31,6 +47,11 @@ function Home(): JSX.Element {
 
             { desserts.map(dessert => <span key={dessert.id}>{dessert.name} ${dessert.price} | </span>) }
 
+            <br/>
+            <button onClick={displaySale1Info}>Sale 1 Info</button>
+            <button onClick={displaySale2Info}>Sale 2 Info</button>
+            <p>sale</p>
+            <p>{saleInfo}</p>
         </div>
     )
 }
